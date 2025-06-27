27264e65c74a0aae1e51afc45675dbd6a64ba391
Flexagon <105828046+srimukhiPerepa@users.noreply.github.com>
2025-06-27 16:32:04 +0530

    Create InsertData.sql

---
+++ InsertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO Persons VALUES (123, 'Tom B', ' Erichsen', 'Dallas', 'Texas');
+INSERT INTO Persons VALUES (124, 'Tammy', ' Lawson', 'US', 'California');
+INSERT INTO Persons VALUES (125, 'Brian', ' Collins', 'GreenBay', 'Wisconsin');
